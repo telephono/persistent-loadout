b95f942dc458869cb1eb2b0f6013920b735f1113
telephono <telephono@users.noreply.github.com>
2024-09-22 19:13:37 +0200

    Add autobrake, autothrottle and navigation equipment to loadout

--- src/loadout.rs
+++ src/loadout.rs
@@ -13,6 +13,9 @@ use crate::plugin::{PluginError, LOADOUT_FILENAME};
 #[derive(Serialize, Deserialize)]
 struct Loadout {
     m_fuel: Vec<f32>,
+    autobrake: f32,
+    autothrottle: f32,
+    navigation: f32,
 }
 
 pub struct LoadoutData {
@@ -66,9 +69,30 @@ impl LoadoutData {
     fn get_from_sim(mut self) -> Result<Self, PluginError> {
         debugln!("getting loadout from X-Plane");
         let m_fuel: DataRef<[f32], ReadOnly> = DataRef::find("sim/flightmodel/weight/m_fuel")?;
+        let generic_lights_switch: DataRef<[f32], ReadOnly> = DataRef::find("sim/cockpit2/switches/generic_lights_switch")?;
+
+        let generic_lights_switch = generic_lights_switch.as_vec();
+
+        let autothrottle = match generic_lights_switch.get(49) {
+            Some(&autthrottle) => autthrottle,
+            None => 0.0,
+        };
+
+        let autobrake = match generic_lights_switch.get(50) {
+            Some(&autobrake) => autobrake,
+            None => 0.0
+        };
+
+        let navigation = match generic_lights_switch.get(84) {
+            Some(&navigation) => navigation,
+            None => 0.0,
+        };
 
         let loadout = Loadout {
-            m_fuel: m_fuel.as_vec()
+            m_fuel: m_fuel.as_vec(),
+            autobrake,
+            autothrottle,
+            navigation,
         };
 
         self.loadout = Some(loadout);
@@ -80,8 +104,21 @@ impl LoadoutData {
             debugln!("setting loadout in X-Plane");
             let mut m_fuel: DataRef<[f32], ReadWrite> = DataRef::find("sim/flightmodel/weight/m_fuel")?
                 .writeable()?;
+            let mut generic_lights_switch: DataRef<[f32], ReadWrite> = DataRef::find("sim/cockpit2/switches/generic_lights_switch")?
+                .writeable()?;
 
-            // Write fuel levels into sim
+            // Write equipment into sim...
+            let mut new_generic_lights_switch = generic_lights_switch.as_vec();
+            if new_generic_lights_switch.len() >= 128 {
+                new_generic_lights_switch[49] = loadout.autothrottle;
+                new_generic_lights_switch[50] = loadout.autobrake;
+                new_generic_lights_switch[84] = loadout.navigation;
+                generic_lights_switch.set(new_generic_lights_switch.as_slice());
+            } else {
+                debugln!("sim/cockpit2/switches/generic_lights_switch did not return at least 128 entries, skipping equipment setup");
+            }
+
+            // Write fuel levels into sim...
             let new_m_fuel = loadout.m_fuel.as_slice();
             m_fuel.set(new_m_fuel);
         }
