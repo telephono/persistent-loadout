f63b99526320b88329b12b2831c7f55b4910afcc
telephono <telephono@users.noreply.github.com>
2024-10-03 15:03:55 +0200

    Add XPLMGetNthAircraftModel wrapper

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,6 +1,10 @@
 // Copyright (c) 2024 telephono
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
+use std::ffi::{c_char, c_int, CStr, OsString};
+use std::fmt::Display;
+use std::path::PathBuf;
+
 use thiserror::Error;
 
 use xplm::data::borrowed::DataRef;
@@ -20,6 +24,8 @@ pub static LOADOUT_FILENAME: &str = "persistent-loadout.json";
 pub enum PluginError {
     #[error(transparent)]
     InputOutput(#[from] std::io::Error),
+    #[error(transparent)]
+    Utf8Error(#[from] std::str::Utf8Error),
     #[error(transparent)]
     FromUtf8Error(#[from] std::string::FromUtf8Error),
     #[error(transparent)]
@@ -91,5 +97,72 @@ impl Plugin for PersistentLoadoutPlugin {
             signature: String::from(SIGNATURE),
             description: String::from(DESCRIPTION),
         }
+    }
+}
+
+/// XPLMGetNthAircraftModel wrapper
+#[derive(Debug)]
+pub struct AircraftModel {
+    out_file: PathBuf,
+    out_path: PathBuf,
+}
+
+impl AircraftModel {
+    pub fn new(index: c_int) -> Result<Self, PluginError> {
+        let mut out_file_buf: [c_char; 256] = [b'\0' as c_char; 256];
+        let mut out_path_buf: [c_char; 512] = [b'\0' as c_char; 512];
+        let out_file: &CStr;
+        let out_path: &CStr;
+
+        unsafe {
+            xplm_sys::XPLMGetNthAircraftModel(
+                index as c_int,
+                out_file_buf.as_mut_ptr(),
+                out_path_buf.as_mut_ptr(),
+            );
+
+            out_file = CStr::from_ptr(out_file_buf.as_ptr());
+            out_path = CStr::from_ptr(out_path_buf.as_ptr());
+        }
+
+        let out_file = PathBuf::from(out_file.to_str()?);
+        let out_path = PathBuf::from(out_path.to_str()?);
+
+        Ok(Self { out_file, out_path })
+    }
+
+    /// Return aircraft's acf file name without .acf extension
+    pub fn out_file_stem(&self) -> OsString {
+        self.out_file.file_stem().unwrap_or_default().to_owned()
+    }
+
+    /// Return path to aircraft's acf file
+    /// The path is relative to X-Plane root
+    pub fn relative_out_path(&self) -> PathBuf {
+        // Turn `out_path` into a relative path starting with the `Aircraft` directory
+        let mut relative = false;
+
+        let mut out_path: PathBuf = self
+            .out_path
+            .iter()
+            .filter(|&segment| {
+                if segment == "Aircraft" {
+                    relative = true;
+                }
+                relative
+            })
+            .collect();
+
+        // Truncate file name from path
+        out_path.pop();
+
+        out_path
+    }
+}
+
+impl Display for AircraftModel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let out_path_file = self.out_path.join(self.out_file.as_path());
+        write!(f, "{}", out_path_file.to_string_lossy())
     }
 }
